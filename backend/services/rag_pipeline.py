cf1011982c0c4bf521a33aaae68bd06e28eeae65
kardi.ib <kardi.ib@gmail.com>
2025-06-15 19:46:42 +0700

    fix(backend rag pipelin): fix logic for retrive answer from ai

--- backend/services/rag_pipeline.py
+++ backend/services/rag_pipeline.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Any
 from langchain.schema import Document
 from services.vector_store import VectorStoreService
 from config import settings
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI
 import logging
 
 logger = logging.getLogger(__name__)
@@ -15,8 +15,8 @@ class RAGPipeline:
         # - LLM client
         # - Prompt templates
         self.vector_store = vector_store
-        self.llm = OpenAI(
-            model=settings.llm_model, 
+        self.llm = ChatOpenAI(
+            model_name=settings.llm_model, 
             openai_api_key=settings.openai_api_key,
             temperature=settings.llm_temperature,
             max_tokens=settings.max_tokens
@@ -27,7 +27,7 @@ class RAGPipeline:
         # TODO: Implement RAG pipeline
         # 1. Retrieve relevant documents
         docs_with_scores = self.vector_store.similarity_search(question,k=settings.retrieval_k)
-        docs = [d[0] for d in docs_with_scores if d[1] > settings.similarity_threshold]
+        docs = [d[0] for d in docs_with_scores if d[1] < settings.similarity_threshold]
         # 2. Generate context from retrieved documents
         context = "\n\n".join([f"(page {doc.metadata.get('page', '?')}): {doc.page_content}" for doc in docs])
         prompt = (
@@ -37,12 +37,12 @@ class RAGPipeline:
             "Answer (use only the provided context and cite page numbers):"
         )
         # 3. Generate answer using LLM
-        answer = self.llm(prompt)
+        answer = self.llm.invoke(prompt).content
         sources = [
             {
                 "content": doc.page_content,
                 "page": doc.metadata.get("page", "?"),
-                "score": doc.metadata.get("score", "?")
+                "score": score
             }
             for doc, score in docs_with_scores
         ]
